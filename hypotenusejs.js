3088367fa0d376723edce7309a8ac3df5cccd6ab
Himanshu <88453380+himanshu0089@users.noreply.github.com>
2024-09-18 23:38:51 +0530

    Update hypotenusejs.js

--- hypotenusejs.js
+++ hypotenusejs.js
@@ -5,8 +5,11 @@ var output=document.querySelector("#output-box");
 var hypotenuse;
 check.addEventListener("click", function findHypotenuse()
 {
+       if(base==0 || height==0){
+              return 0 }
+       else{
        hypotenuse=(base.value*base.value+height.value*height.value);
       hypotenuse=Math.sqrt(hypotenuse); 
      //console.log(hypotenuse);
-   output.innerText=hypotenuse;
+   output.innerText=hypotenuse;}
 });
